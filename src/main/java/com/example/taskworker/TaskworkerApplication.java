ff7ea6848e0f694d57d5f7ceab5cb7680e21f330
lyubovnikov <lyubovnikov@lanit.ru>
2025-05-24 19:18:42 +0700

    Initial commit: Spring Boot project

---
+++ src/main/java/com/example/taskworker/TaskworkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskworkerApplication.class, args);
+	}
+
+}
